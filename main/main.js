89af4bce1ffedf1daebf66209abe7e4431e19f25
SiruoLiu <1143398076@qq.com>
2017-05-21 14:26:25 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,7 +1,7 @@
 module.exports = function main(inputs) {
-    console.log("Debug Info");
+  //  console.log("Debug Info");
     var num=inputs.split("");
-      console.log(num[0]);
+    //  console.log(num[0]);
     var mArray = [["...","...","..."],["...","...","..."],["...","...","..."]];
     var tArray = new Array();  //先声明一维
     for(var k=0;k<3;k++){    //一维长度为i,i为变量，可以根据实际情况改
