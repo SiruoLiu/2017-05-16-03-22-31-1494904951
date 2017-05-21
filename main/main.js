5ccbab9172e2cb79553f145a98859c4b559e5666
SiruoLiu <1143398076@qq.com>
2017-05-22 00:11:55 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -3,7 +3,7 @@ module.exports = function main(inputs) {
     var a=inputs;
   var num=a.split("");
     //  console.log(num[0]);
-    var mArray = [[".",".","."],[".",".","."],[".",".","."]];
+   
     var tArray = new Array();  //先声明一维
     for(var k=0;k<3;k++){    //一维长度为i,i为变量，可以根据实际情况改
        tArray[k]=new Array();  //声明二维，每一个一维数组里面的一个元素都是一个数组；
@@ -11,7 +11,8 @@ module.exports = function main(inputs) {
        tArray[k][j]="";    //这里将变量初始化，我这边统一初始化为空，后面在用所需的值覆盖里面的值
          }
     for(var i=0;i<num.length;i++)
-    {
+    { 
+      var mArray = [[".",".","."],[".",".","."],[".",".","."]];
         var item=parseInt(num[i]);
      switch (item)
     {case 0:
