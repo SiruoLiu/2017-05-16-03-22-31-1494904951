26e2eba7ae3922e99583663c4028e0a8773c6468
SiruoLiu <1143398076@qq.com>
2017-05-21 12:54:52 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,77 @@
-module.exports = function main() {
+module.exports = function main(inputs) {
     console.log("Debug Info");
+    var num=inputs.split("");
+    var mArray = [["...","...","..."],["...","...","..."],["...","...","..."]];
+    switch (num):
+    {case 0:
+             mArray[0][1]="_";
+             mArray[1][0]="|";
+             mArray[1][2]="|";
+             mArray[2][0]="|";
+             mArray[2][1]="_";
+             mArray[2][2]="|";
+            break;
+     case 1:
+             mArray[1][2]="|";
+             mArray[2][2]="|";
+            break;
+     case 2:
+             mArray[0][1]="_";
+             mArray[1][1]="_";
+             mArray[1][2]="|";
+             mArray[2][0]="|";
+             mArray[2][1]="_";
+            break;
+     case 3:
+             mArray[0][1]="_";
+             mArray[1][1]="_";
+             mArray[1][2]="|";
+             mArray[2][2]="|";
+             mArray[2][1]="_";
+            break;
+     case 4:
+            mArray[1][0]="|";
+             mArray[1][1]="_";
+             mArray[1][2]="|";
+             mArray[2][2]="|";
+            break;
+     case 5:
+             mArray[0][1]="_";
+             mArray[1][1]="_";
+             mArray[1][0]="|";
+             mArray[2][2]="|";
+             mArray[2][1]="_";
+            break;
+     case 6:
+             mArray[0][1]="_";
+             mArray[1][1]="_";
+             mArray[1][0]="|";
+            mArray[2][0]="|";
+             mArray[2][2]="|";
+             mArray[2][1]="_";
+            break;
+     case 7:
+             mArray[0][1]="_";
+            mArray[1][2]="|";
+             mArray[2][2]="|";
+            break;
+     case 8:
+            mArray[0][1]="_";
+             mArray[1][0]="|";
+            mArray[1][1]="_";
+             mArray[1][2]="|";
+             mArray[2][0]="|";
+             mArray[2][1]="_";
+             mArray[2][2]="|";
+            break;
+     case 9:
+            mArray[0][1]="_";
+             mArray[1][0]="|";
+            mArray[1][1]="_";
+             mArray[1][2]="|";
+             mArray[2][2]="|";
+            break;
+      }
+    
     return 'Hello World!';
 };
