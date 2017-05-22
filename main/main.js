01922af53179da2f7396a3960f2d5cb7faf945b7
SiruoLiu <1143398076@qq.com>
2017-05-22 09:21:05 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -7,7 +7,7 @@ module.exports = function main(inputs) {
     var tArray = new Array();  //先声明一维
     for(var k=0;k<3;k++){    //一维长度为i,i为变量，可以根据实际情况改
        tArray[k]=new Array();  //声明二维，每一个一维数组里面的一个元素都是一个数组；
-       for(var j=0;j<3*num.length;j++){   //一维数组里面每个元素数组可以包含的数量p，p也是一个变量；
+       for(var j=0;j<4*num.length;j++){   //一维数组里面每个元素数组可以包含的数量p，p也是一个变量；
        tArray[k][j]="";    //这里将变量初始化，我这边统一初始化为空，后面在用所需的值覆盖里面的值
          }
         }
@@ -85,15 +85,18 @@ module.exports = function main(inputs) {
              mArray[2][2]="|";
             break;
       }
-     tArray[0][i*3+0]=mArray[0][0];
-        tArray[0][i*3+1]=mArray[0][1];
-        tArray[0][i*3+2]=mArray[0][2];
-        tArray[1][i*3+0]=mArray[1][0];
-        tArray[1][i*3+1]=mArray[1][1];
-        tArray[1][i*3+2]=mArray[1][2];
-        tArray[2][i*3+0]=mArray[2][0];
-        tArray[2][i*3+1]=mArray[2][1];
-        tArray[2][i*3+2]=mArray[2][2];
+     tArray[0][i*4+0]=mArray[0][0];
+        tArray[0][i*4+1]=mArray[0][1];
+        tArray[0][i*4+2]=mArray[0][2];
+        tArray[0][i*4+3]=" ";
+        tArray[1][i*4+0]=mArray[1][0];
+        tArray[1][i*4+1]=mArray[1][1];
+        tArray[1][i*4+2]=mArray[1][2];
+        tArray[1][i*4+3]=" ";
+        tArray[2][i*4+0]=mArray[2][0];
+        tArray[2][i*4+1]=mArray[2][1];
+        tArray[2][i*4+2]=mArray[2][2];
+        tArray[2][i*4+3]=" ";
      }
     
    var print=new Array();
